4164ab05df98dd784931874bd3c11f59c36900cc
Nayeli A. Silva <117576565+NAS-Ragde@users.noreply.github.com>
2025-04-21 14:33:31 +0200

    Initial Spring Boot
    Initial Spring Boot with Kotlin setup

---
+++ src/main/kotlin/com/example/wearables_demo/WearablesDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.wearables_demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WearablesDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<WearablesDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/wearables_demo/WearablesDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.wearables_demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WearablesDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
